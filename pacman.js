5b5078e346030d1b3a67a1b6f9ef8b5bfab752de
hangpierr <hangpierr@hotmail.fr>
2021-10-21 16:49:26 +0200

    finjeudisoir

---
+++ pacman.js
@@ -0,0 +1,61 @@
+let grille=[
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    [0,2,2,2,2,2,2,2,2,0,2,2,2,2,2,2,2,2,0],
+    [0,2,0,0,2,0,0,0,2,0,2,0,0,0,2,0,0,2,0],
+    [0,2,0,0,2,0,0,0,2,0,2,0,0,0,2,0,0,2,0],
+    [0,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,0],
+    [0,2,0,0,2,0,2,0,0,0,0,0,2,0,2,0,0,2,0],
+    [0,2,2,2,2,0,2,2,2,0,2,2,2,0,2,2,2,2,0],
+    [0,0,0,0,2,0,0,0,2,0,2,0,0,0,2,0,0,0,0],
+    [0,1,1,0,2,0,2,2,2,2,2,2,2,0,2,0,1,1,0],
+    [0,0,0,0,2,0,2,0,0,1,0,0,2,0,2,0,0,0,0],
+    [2,2,2,2,2,2,2,0,1,1,1,0,2,2,2,2,2,2,2],
+    [0,0,0,0,2,0,2,0,0,1,0,0,2,0,2,0,0,0,0],
+    [0,1,1,0,2,0,2,2,2,2,2,2,2,0,2,0,1,1,0],
+    [0,0,0,0,2,0,2,0,0,0,0,0,2,0,2,0,0,0,0],
+    [0,2,2,2,2,2,2,2,2,0,2,2,2,2,2,2,2,2,0],
+    [0,2,0,0,2,0,0,0,2,0,2,0,0,0,2,0,0,2,0],
+    [0,2,2,0,2,2,2,2,2,2,2,2,2,2,2,0,2,2,0],
+    [0,0,2,0,2,0,2,0,0,0,0,0,2,0,2,0,2,0,0],
+    [0,2,2,2,2,0,2,2,2,0,2,2,2,0,2,2,2,2,0],
+    [0,2,0,0,0,0,0,0,2,0,2,0,0,0,0,0,0,2,0],
+    [0,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,0],
+    [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+]
+let pacman={
+    x:10,
+    y:11,
+    direction:0
+}
+function afficheGrille() {
+    document.getElementById("grille").innerHTML=""
+for(let i=0; i<22;i++){
+for(let j=0; j<19;j++){
+    let elem=document.createElement("div")
+    elem.style.gridColumnStart=j+1
+    elem.style.gridRowStart=i+1
+    if(grille[i][j]==0){
+    elem.className="mur"
+    }else if(grille[i][j]==1){
+        elem.className="sol"
+    }else{
+        elem.className="bonbon"
+    }
+    document.getElementById("grille").appendChild(elem);
+}
+    
+}
+}
+setInterval(tourDeJeu,500)
+
+function tourDeJeu(){
+    afficheGrille();
+    affichePacman();
+}
+function affichePacman() {
+    let elem=document.createElement("div");
+    document.getElementById("grille").appendChild(elem);
+    elem.className="pacman";
+    elem.style.gridColumnStart=pacman.x
+    elem.style.gridRowStart=pacman.y
+}
