e90dabe1c0a6bf6305b717516f8465f3f4cca1e8
hangpierr <hangpierr@hotmail.fr>
2021-11-04 16:29:10 +0100

    04/11 fin de journee

--- pacman.js
+++ pacman.js
@@ -22,17 +22,39 @@ let grille=[
     [0,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,0],
     [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
 ]
+
+let copygrille=JSON.parse(JSON.stringify(grille))
+
 document.body.addEventListener("keypress",changeDirection)
+document.getElementById("rejouer").addEventListener("click", rejouer)
 let pacman={
     x:5,
     y:2,
     direction:0
 }
-let fantomebleu={
+
+// TABLEAU //
+
+let tableaufantome1=[{
     x:10,
     y:11,
     direction:0
-}
+}, {
+    x:10,
+    y:11,
+    direction:0
+}, {
+    x:10,
+    y:11,
+    direction:0
+}, {
+    x:10,
+    y:11,
+    direction:0
+}]
+
+let copyfantome=JSON.parse(JSON.stringify(tableaufantome1))
+
 let score = 0
 
 function afficheGrille() {
@@ -50,23 +72,35 @@ for(let j=0; j<19;j++){
         elem.className="bonbon"
     }
     document.getElementById("grille").appendChild(elem);
-}
-    
+}  
 }
 }
 let numInterval=setInterval(tourDeJeu,300)
 
+// TOUR DE JEU //
+
 function tourDeJeu(){
     afficheGrille();
+    bougepacman();
+    collision();
     sorties();
     mangerbonbon();
     affichePacman();
-    afficheFantomebleu();
-    bougepacman();
-    collision();
     afficherscore();
     gagner();
+    for(i=0;i<tableaufantome1.length;i++){
+        sortiesfantome1(i);
+        perdu(i);
+        bougefantome1(i);
+        fantomecollision(i);
+        affichefantome1(i);
+        perdu(i);    
+    }
+
 }
+
+// PACMAN //
+
 function affichePacman() {
     let elem=document.createElement("div");
     document.getElementById("grille").appendChild(elem);
@@ -104,8 +138,16 @@ function collision() {
             pacman.x = pacman.x+1
         }else if (pacman.direction == 2){
             pacman.y = pacman.y-1
-        }else{ pacman.y = pacman.y+1
+        }else if (pacman.direction ==3){
+             pacman.y = pacman.y+1
         }
+    }
+}
+function sorties() {
+    if (pacman.x < 1) {
+        pacman.x = 19 
+    }if (pacman.x > 19) {
+        pacman.x = 1
     }
 }
 function mangerbonbon() {
@@ -115,16 +157,12 @@ function mangerbonbon() {
         score+=10 //score=score+10
     }
 }
+
+// ALERT INFO GAME //
+
 function afficherscore() {
     document.getElementById("score").innerHTML="score:"+score
 
-}
-function sorties() {
-    if (pacman.x < 1) {
-        pacman.x = 19 
-    }if (pacman.x > 19) {
-        pacman.x = 1
-    }
 }
 function gagner() {
     let gagner = true
@@ -136,14 +174,69 @@ function gagner() {
 }
     }
     if (gagner==true){
-        alert("vous avez gagne")
+        prompt("vous avez gagne")
         clearInterval(numInterval) 
     }
 }
-function afficheFantomebleu() {
+function perdu(num) {
+    if (tableaufantome1[num].x==pacman.x && tableaufantome1[num].y == pacman.y) {
+        prompt("vous avez perdu")
+        clearInterval(numInterval)
+    }
+}
+function rejouer() {
+    clearInterval(numInterval)
+    grille=JSON.parse(JSON.stringify(copygrille))
+    pacman.x=5
+    pacman.y=2
+    pacman.direction=0
+    tableaufantome1=JSON.parse(JSON.stringify(copyfantome))
+    numInterval=setInterval(tourDeJeu,300)
+    score=0
+}
+
+
+// FANTOME BLEU//
+
+function affichefantome1(num) {
     let elem=document.createElement("div");
     document.getElementById("grille").appendChild(elem);
-    elem.className="fantomebleu";
-    elem.style.gridColumnStart=fantomebleu.x
-    elem.style.gridRowStart=fantomebleu.y
+    elem.className="fantome"+(num%4);
+    elem.style.gridColumnStart=tableaufantome1[num].x
+    elem.style.gridRowStart=tableaufantome1[num].y
+}
+function getRandomInt(max) {
+    return Math.floor(Math.random()*max)
+}
+function bougefantome1(num) {
+    tableaufantome1[num].direction = getRandomInt(4);
+    if (tableaufantome1[num].direction == 0){
+        tableaufantome1[num].x = tableaufantome1[num].x+1
+    }else if (tableaufantome1[num].direction == 1){
+        tableaufantome1[num].x = tableaufantome1[num].x-1
+    }else if (tableaufantome1[num].direction == 2){
+        tableaufantome1[num].y = tableaufantome1[num].y+1
+    }else if (tableaufantome1[num].direction == 3){
+        tableaufantome1[num].y = tableaufantome1[num].y-1
+    }
+}
+function fantomecollision(num) {
+    if (grille[tableaufantome1[num].y-1][tableaufantome1[num].x-1]==0) {
+        if (tableaufantome1[num].direction == 0){
+            tableaufantome1[num].x = tableaufantome1[num].x-1
+        }else if (tableaufantome1[num].direction == 1){
+            tableaufantome1[num].x = tableaufantome1[num].x+1
+        }else if (tableaufantome1[num].direction == 2){
+            tableaufantome1[num].y = tableaufantome1[num].y-1
+        }else if (tableaufantome1[num].direction ==3){
+            tableaufantome1[num].y = tableaufantome1[num].y+1
+        }
+    }
+}
+function sortiesfantome1(num) {
+    if (tableaufantome1[num].x < 1) {
+        tableaufantome1[num].x = 19 
+    }if (tableaufantome1[num].x > 19) {
+        tableaufantome1[num].x = 1
+    }
 }
